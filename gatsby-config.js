5c71d687fd272b576bcf1d74ac9cda194f8b595b
justFlow5 <justflow555@gmail.com>
2019-11-03 20:16:14 +0100

    json fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,13 +16,13 @@ module.exports = {
     // "gatsby-transformer-inline-svg",
     "gatsby-plugin-sass",
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
