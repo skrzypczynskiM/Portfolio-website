375fc27c3eef877ea53b467582c06d48edcdfdfb
Michał Skrzypczyński <justflow555@gmail.com>
2020-03-16 23:26:54 +0100

    change 404 pics path

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,12 @@ import { Link } from "gatsby"
 import Head from "../components/head"
 import Layout from "../components/layout"
 
+import Rocket from "../images/rocket.svg"
+import Earth from "../images/earth.svg"
+import Astronaut from "../images/astronaut.svg"
+import Moon from "../images/moon.svg"
+import Page404 from "../images/404.svg"
+
 import styles404 from "./404.module.scss"
 
 const NotFound = () => {
@@ -15,7 +21,8 @@ const NotFound = () => {
           <div className={styles404.centralBody}>
             <img
               className={styles404.image404}
-              src="http://salehriaz.com/404Page/img/404.svg"
+              // src="http://salehriaz.com/404Page/img/404.svg"
+              src={Page404}
               width={300}
             />
             <Link
@@ -29,25 +36,29 @@ const NotFound = () => {
           <div className={styles404.objects}>
             <img
               className={styles404.object_rocket}
-              src="http://salehriaz.com/404Page/img/rocket.svg"
+              // src="http://salehriaz.com/404Page/img/rocket.svg"
+              src={Rocket}
               width={40}
             />
             <div className={styles404.earthMoon}>
               <img
                 className={styles404.object_earth}
-                src="http://salehriaz.com/404Page/img/earth.svg"
+                // src="http://salehriaz.com/404Page/img/earth.svg"
+                src={Earth}
                 width={100}
               />
               <img
                 className={styles404.object_moon}
-                src="http://salehriaz.com/404Page/img/moon.svg"
+                // src="http://salehriaz.com/404Page/img/moon.svg"
+                src={Moon}
                 width={80}
               />
             </div>
             <div className={styles404.box_astronaut}>
               <img
                 className={styles404.box_astronaut}
-                src="http://salehriaz.com/404Page/img/astronaut.svg"
+                // src="http://salehriaz.com/404Page/img/astronaut.svg"
+                src={Astronaut}
                 width={140}
               />
             </div>
