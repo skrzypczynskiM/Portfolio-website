fb463dd85cd993648f69621f3c54e2efba71b508
Michał Skrzypczyński <justflow555@gmail.com>
2020-10-20 12:00:42 +0200

    Fix English gramatic errors in aboutMe section

--- src/components/aboutPage.js
+++ src/components/aboutPage.js
@@ -29,22 +29,21 @@ const AboutPage = () => {
             ></div>
             <div className={aboutStyles.theBeginnings}>
               <p>
-                My journey with programming started in 2015, alongside with
-                studying Bioinformatics major at Wrocław University of
-                Environmental and Life Sciences.{" "}
+                My journey with programming started in 2015, alongside studying
+                Bioinformatics major at Wrocław University of Environmental and
+                Life Sciences.
               </p>
 
               <p>
-                {" "}
                 My first Hello World came from Python 2.7 console. At this very
-                moment the world of programming became wide open for me and I
-                knew what I'm going to do for a living. Along the way I got to
-                learn some basics of R, C, C#, Bash and Java.{" "}
+                moment, the world of programming became wide open for me and I
+                knew what I'm going to do for a living. Along the way, I got to
+                learn some basics of R, C, C#, Bash, and Java.
               </p>
               <p>
                 Each of these languages had its perks (except C maybe ;) ) but
                 it wasn't until I discovered JavaScript when I truly became
-                passionate about programming and Web Development in general.{" "}
+                passionate about programming and Web Development in general.
               </p>
             </div>
           </div>
